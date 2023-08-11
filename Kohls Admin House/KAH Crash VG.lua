de3a4705f6247681175a4c4142678ff899d9fde0
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-11 17:52:46 +0100

    Update and rename KAH Crash V2.lua to KAH Crash VG.lua

--- Kohls Admin House/KAH Crash VG.lua
+++ Kohls Admin House/KAH Crash VG.lua
@@ -1,3 +1,4 @@
+--bad but it works sometimes ig
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-31.0896435, 8.22999477, 70.522644, -0.999961913, 4.495271e-08, -0.0087288795, 4.55292621e-08, 1, -6.58523618e-08, 0.0087288795, -6.62472743e-08, -0.999961913)
 scriptActive=true
 padAbuse=true
