0b63e7722ee3c53c72fb48d74ca55fcb63a2fe68
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-11 17:57:34 +0100

    Create anticrashjo.lua

---
+++ Kohls Admin House/anticrashjo.lua
@@ -0,0 +1,7 @@
+local counter = 0
+while counter < 1000 do -- adjust number
+  game.Players:Chat('removeclones')
+  game.Players:Chat('undog all')
+  game.Players:Chat('punish all')
+  counter = counter + 1
+end
