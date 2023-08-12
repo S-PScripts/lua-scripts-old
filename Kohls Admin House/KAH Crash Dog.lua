01f917e53c7d5a1d840d8a302a27b39a3b3351a1
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-12 17:54:38 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/KAH Crash Dog.lua
+++ Kohls Admin House/KAH Crash Dog.lua
@@ -4,7 +4,7 @@ game.Players:Chat('fogend 0')
 game.Players:Chat('flash')
 game.Players:Chat('clr')
 game.Players:Chat('time 0')
-game.Players:Chat('h \n\n\n [agspureiam]: The server has been shutdown. \n\n\n')
+game.Players:Chat('h \n\n\n 6 lines of main code crashed this server. \n\n\n')
 game.Players:Chat('music 0000000000000000000006680495507')
 wait(0.5)
 local counter = 0
