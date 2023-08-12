fde313af287470bf84f9a4a2e1ad2f46d89d7661
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-12 10:39:28 +0100

    Update KAH Crash jotunn.lua

--- Kohls Admin House/KAH Crash jotunn.lua
+++ Kohls Admin House/KAH Crash jotunn.lua
@@ -1,6 +1,6 @@
 game.Players:Chat('blind all')
 game.Players:Chat('h \n\n\n [agspureiam]: The server has been shutdown. \n\n\n')
-game.Players:Chat('music 0000000000000000000006529070845')
+game.Players:Chat('music 0000000000000000000006680495507')
 wait(0.5)
 local counter = 0
 while counter < 1000 do
@@ -9,4 +9,4 @@ while counter < 1000 do
   counter = counter + 1
 end
 
-game.Players:Chat('music 0000000000000000000006680495507')
+game.Players:Chat('music 0000000000000000000006529070845')
