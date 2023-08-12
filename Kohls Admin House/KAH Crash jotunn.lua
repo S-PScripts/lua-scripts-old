9c13c17f8928a32ca304cb6e322997fc6ce12995
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-12 10:32:11 +0100

    Update KAH Crash jotunn.lua

--- Kohls Admin House/KAH Crash jotunn.lua
+++ Kohls Admin House/KAH Crash jotunn.lua
@@ -1,3 +1,7 @@
+game.Players:Chat('blind all')
+game.Players:Chat('h \n\n\n [agspureiam]: The server has been shutdown. \n\n\n')
+game.Players:Chat('music 0000000000000000000006529070845')
+wait(0.5)
 local counter = 0
 while counter < 1000 do
   game.Players:Chat('clone all all all')
