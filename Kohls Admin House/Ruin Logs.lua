6eeca0b24e2d2365129769f98b648623fa4a03e2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-12 10:52:07 +0100

    Update Ruin Logs.lua

--- Kohls Admin House/Ruin Logs.lua
+++ Kohls Admin House/Ruin Logs.lua
@@ -2,5 +2,5 @@ local counter = 0
 while counter < 100 do
   game.Players:Chat('ff \n\n\n NO LOGS FOR YOU! NO LOGS FOR YOU! \n\n\n') -- overlap
   game.Players:Chat('ff \n\n\n NO LOGS FOR YOU! NO LOGS FOR YOU!') -- overlap
-  game.Players:Chat('respawn me') -- cmd bc
+  wait(0.01)
 end
