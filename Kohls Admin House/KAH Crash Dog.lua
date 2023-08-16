3ff370038cfaaff55956c9335d3a775d851df994
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-16 18:02:52 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/KAH Crash Dog.lua
+++ Kohls Admin House/KAH Crash Dog.lua
@@ -7,7 +7,7 @@ game.Players:Chat('time 0')
 game.Players:Chat('h \n\n\n 6 lines of main code crashed this server. \n\n\n')
 game.Players:Chat('music 0000000000000000000009048375035')
 wait(0.5)
-local counter = 0
+counter = 0
 while counter < 1000 do
   game.Players:Chat('clone all all all')
   game.Players:Chat('dog all all all')
