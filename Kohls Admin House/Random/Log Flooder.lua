22776994540888777d54930af6d53449853fd131
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 19:10:03 +0100

    Update and rename Kohls Admin House/Ruin Logs.lua to Kohls Admin House/Random/Log Flooder.lua

---
+++ Kohls Admin House/Random/Log Flooder.lua
@@ -0,0 +1,5 @@
+counter = 0
+while counter < 100 do -- adjust for amount
+  game.Players:Chat('ff NO LOGS FOR YOU!')
+  counter = counter + 1
+end
--- Kohls Admin House/Ruin Logs.lua
+++
@@ -1,6 +0,0 @@
-local counter = 0
-while counter < 100 do
-  game.Players:Chat('ff \n\n\n NO LOGS FOR YOU! NO LOGS FOR YOU! \n\n\n') -- overlap
-  game.Players:Chat('ff \n\n\n NO LOGS FOR YOU! NO LOGS FOR YOU!') -- overlap
-  counter = counter + 1
-end
