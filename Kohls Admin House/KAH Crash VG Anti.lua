145718d15658d5ed0a12bd30310222147f7614c5
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 19:08:25 +0100

    Update and rename Anti-gears to KAH Crash VG Anti.lua

---
+++ Kohls Admin House/KAH Crash VG Anti.lua
@@ -0,0 +1,9 @@
+game:GetService("RunService").RenderStepped:Connect(function()
+        for _, Player in pairs(game.Players:GetChildren()) do
+            if Player.Backpack:FindFirstChild("VampireVanquisher") or Player.Character:FindFirstChild("VampireVanquisher") then
+                game.Players:Chat("removegear ".. Player.Name)
+                game.Players:Chat("refresh ".. Player.Name)
+                game.Players:Chat("h \n\n\n ".. Player.Name.. " just tried to use the Vampire Vanquisher! Why did they do that? \n\n\n")
+            end
+        end
+    end)
