4206e641316f7606fd945f58b35608d8900ed231
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 19:02:17 +0100

    Create KAH Crash Dog Sil.lua

---
+++ Kohls Admin House/Crashes/KAH Crash Dog Sil.lua
@@ -0,0 +1,8 @@
+--ONLY WORKS IN KAH NBC
+--IF YOU DONT HAVE PERM ADD THE KAH PADS.lua CFRAMES TO THIS SCRIPT
+counter = 0
+while counter < 1000 do
+  game.Players:Chat('clone all all all')
+  game.Players:Chat('dog all all all')
+  counter = counter + 1
+end
