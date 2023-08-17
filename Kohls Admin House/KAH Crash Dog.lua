139db0e04e1d66d2db6dc75a08fe215faed14551
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 16:43:10 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/KAH Crash Dog.lua
+++ Kohls Admin House/KAH Crash Dog.lua
@@ -6,7 +6,7 @@ game.Players:Chat('clr')
 game.Players:Chat('time 0')
 game.Players:Chat('blind others')
 game.Players:Chat('h \n\n\n Oops, something went wrong! >_< <jotunn reference> \n\n\n')
-game.Players:Chat('music 0000000000000000000006917155909')
+game.Players:Chat('music 0000000000000000000006529070845')
 wait(0.5)
 counter = 0
 while counter < 1000 do
@@ -15,6 +15,7 @@ while counter < 1000 do
   counter = counter + 1
 end
 
--- 0000000000000000000009048375035
--- 0000000000000000000006680495507
--- 0000000000000000000006529070845
+-- 0000000000000000000009048375035 (all dropping 8 beats)
+-- 0000000000000000000006680495507 (meow meow)
+-- 0000000000000000000006529070845 (loud)
+-- 0000000000000000000006917155909 (they trying to be cray)
