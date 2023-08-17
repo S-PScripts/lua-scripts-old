64ea97e78f4173c92dcd5f623d85b3892fbad5c5
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 19:21:43 +0100

    Update KAH Crash Dog Anti.lua

--- Kohls Admin House/Crashes/KAH Crash Dog Anti.lua
+++ Kohls Admin House/Crashes/KAH Crash Dog Anti.lua
@@ -5,3 +5,10 @@ while counter < 1000 do -- adjust number for length
   game.Players:Chat('punish all')
   counter = counter + 1
 end
+
+-- ALTERNATE VERSION:
+counter = 0
+while counter < 1000 do -- adjust number for length
+  game.Players:Chat('gear all 253519495') -- CHANGE ALL TO USER
+  counter = counter + 1
+end
