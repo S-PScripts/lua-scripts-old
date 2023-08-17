873df6bceb75c09eadc0abea67e2fa2fbaef67e2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 19:02:59 +0100

    Update KAH Crash VG.lua

--- Kohls Admin House/Crashes/KAH Crash VG.lua
+++ Kohls Admin House/Crashes/KAH Crash VG.lua
@@ -5,16 +5,15 @@ game.Players:Chat('clr')
 game.Players:Chat('time 0')
 game.Players:Chat('gear me 00000000000000094794847')
 game.Players:Chat('music 0000000000000006529070845')
-wait(1.5)
+wait(1.5) -- adjust
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "All")
 for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
         if v.name == "VampireVanquisher" then
             v.Parent = game.Players.LocalPlayer.Character
         end
     end
-wait(0.5)
+wait(0.5) --adjust
 game.Players:Chat('h \n\n\n [agspureiam]: The server has been shutdown. \n\n\n')
-wait(0.5)
 local counter = 0
 while counter < 10 do
   game.Players:Chat('unsize me me me')
