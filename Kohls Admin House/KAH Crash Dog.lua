f10de6a32eac8f369ab486ab28fe3e67be676a61
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-17 16:52:47 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/KAH Crash Dog.lua
+++ Kohls Admin House/KAH Crash Dog.lua
@@ -6,7 +6,7 @@ game.Players:Chat('clr')
 game.Players:Chat('time 0')
 game.Players:Chat('blind others')
 game.Players:Chat('h \n\n\n Oops, something went wrong! >_< <jotunn reference> \n\n\n')
-game.Players:Chat('music 0000000000000000000006529070845')
+game.Players:Chat('music 0000000000000000000006680495507')
 wait(0.5)
 counter = 0
 while counter < 1000 do
