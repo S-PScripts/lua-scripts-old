a22640a5294fdc7cf160c544b09b3a8ffa71b631
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-18 10:25:46 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/Crashes/KAH Crash Dog.lua
+++ Kohls Admin House/Crashes/KAH Crash Dog.lua
@@ -7,17 +7,17 @@ game.Players:Chat('clr')
 game.Players:Chat('time 0')
 game.Players:Chat('blind others')
 if number==1 then
-  game.Players:Chat('h \n\n\n Sorry, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n Sorry, something went wrong! {ERROR 1} \n\n\n')
 elseif number==2 then
-  game.Players:Chat('h \n\n\n Oops, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n Oops, something went wrong! {ERROR 2} \n\n\n')
 elseif number==3 then
-  game.Players:Chat('h \n\n\n [ROBLOX] Oops, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n [ROBLOX] Oops, something went wrong! {ERROR 3} \n\n\n')
 elseif number==4 then
-  game.Players:Chat('h \n\n\n [Agspureiam] Oops, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n [Agspureiam] Oops, something went wrong! {ERROR 4} \n\n\n')
 elseif number== 5 then
-  game.Players:Chat('h \n\n\n [ROBLOX] Sorry, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n [ROBLOX] Sorry, something went wrong! {ERROR 5} \n\n\n')
 else
-  game.Players:Chat('h \n\n\n [Agspureiam] Sorry, something went wrong! \n\n\n')
+  game.Players:Chat('h \n\n\n [Agspureiam] Sorry, something went wrong! {ERROR 6} \n\n\n')
 end
 game.Players:Chat('music 0000000000000000000006529070845')
 wait(0.5)
