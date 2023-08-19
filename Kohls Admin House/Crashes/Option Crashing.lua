225696182657cc4cb2c82ec7b30523c1b7af80b0
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-19 22:25:27 +0100

    Create Option Crashing.lua

---
+++ Kohls Admin House/Crashes/Option Crashing.lua
@@ -0,0 +1,16 @@
+local crash = freeze
+if crash == freeze then
+        for i = 1, 1750 do
+            game.Players:Chat('freeze '..string.rep(' all', 40))
+            game.Players:Chat('clone all all all')
+            game.Players:Chat('removeclones')
+            task.wait()
+        end
+end
+if crash == dog then
+        for i = 1, 1750 do
+            game.Players:Chat('dog all all all')
+            game.Players:Chat('clone all all all')
+            task.wait()
+        end
+end
