509f4bc9c32e570025f88ee324419dfd4643f8c9
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-19 21:42:46 +0100

    Update KAH Pads 2.lua

--- Kohls Admin House/Pads/KAH Pads 2.lua
+++ Kohls Admin House/Pads/KAH Pads 2.lua
@@ -1,43 +1,19 @@
-padAbuse=true
-task.spawn(function()
-    while true do task.wait() -- It uses while true do instead now
-        coroutine.wrap(function() -- PadAbuse
-			if padAbuse == true then
-				local pads = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:GetChildren("Head")
-				for i, pad in pairs(pads) do
-					coroutine.wrap(function()
-						pcall(function()
-							local cre = pad.Head
-							local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
-							firetouchinterest(cre, spr, 1)
-                            firetouchinterest(cre, spr, 0)
-                            firetouchinterest(cre, spr, 1)
-							task.wait()
-							firetouchinterest(cre, spr, 0)
-							
-							if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
-								fireclickdetector(adminFlr.Regen.ClickDetector, 0)
-							end
-						end)
-					end)()
-				end
-			end
-        end)()
-		
-        coroutine.wrap(function() -- Perm
-			local spr = game.Players.LocalPlayer.Character:FindFirstChild("Head")
-			if perm ~= true or adminFlr.Pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") ~= nil then 
-			else
-				pcall(function()
-					local pad = adminFlr.Pads:FindFirstChild("Touch to get admin")
-					local a = pad.Head
-					firetouchinterest(a, spr, 1)
-                    firetouchinterest(a, spr, 0)
-                    firetouchinterest(a, spr, 1)
-					task.wait()
-					firetouchinterest(a, spr, 0)
-				end)
-			end
-        end)()
-    end
-end)
+perm=true
+while perm==true do
+   if not game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild(game.Players.LocalPlayer.Name .. "'s admin") then
+      if game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin") then
+         local pad = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head")
+         local padCFrame = game:GetService("Workspace").Terrain["_Game"].Admin.Pads:FindFirstChild("Touch to get admin"):FindFirstChild("Head").CFrame
+         wait(0.125)
+         pad.CanCollide = false
+         repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+            pad.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+            wait(0.125)
+            pad.CFrame = padCFrame
+            pad.CanCollide = true
+         else
+            fireclickdetector(game:GetService("Workspace").Terrain["_Game"].Admin.Regen.ClickDetector, 0)
+         end
+      end
+      wait(0.2)
+   end
