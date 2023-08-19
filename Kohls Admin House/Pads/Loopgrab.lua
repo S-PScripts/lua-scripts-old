9bd2a0e3605013f0554508bcd0a8fff9c372bbfe
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-19 21:47:10 +0100

    Create Loopgrab.lua

---
+++ Kohls Admin House/Pads/Loopgrab.lua
@@ -0,0 +1,48 @@
+
+wait(5)
+
+
+
+local loopgrab = true
+
+local function LoopGrabPads()
+    if loopgrab then
+        local pads = game.Workspace.Terrain._Game.Admin.Pads:GetChildren()
+        for i, pad in ipairs(pads) do
+            local head = pad:FindFirstChild("Head")
+            local character = game.Players.LocalPlayer.Character
+            local headOfCharacter = character and character:FindFirstChild("Head")
+            
+            if head and headOfCharacter then
+                firetouchinterest(head, headOfCharacter, 1)
+                firetouchinterest(head, headOfCharacter, 0)
+                firetouchinterest(head, headOfCharacter, 1)
+                wait()
+                firetouchinterest(head, headOfCharacter, 0)
+                
+                if pad.Name ~= game.Players.LocalPlayer.Name.."'s admin" then
+                    fireclickdetector(game.Workspace.Terrain._Game.Admin.Regen.ClickDetector, 0)
+                end
+            end
+        end
+    end
+end
+
+game.Players.LocalPlayer.Chatted:Connect(function(message)
+    local lowerMessage = message:lower()
+    
+    if lowerMessage == ".loopgrab" or lowerMessage == ".lg" then
+        loopgrab = true
+        print("Loopgrab enabled.")
+    elseif lowerMessage == ".unloopgrab" or lowerMessage == ".unlg" then
+        loopgrab = false
+        print("Loopgrab disabled.")
+    end
+end)
+
+coroutine.wrap(function()
+    while true do
+        LoopGrabPads()
+        wait(0.001)
+    end
+end)()
