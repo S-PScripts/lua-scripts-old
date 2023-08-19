a8865eb5df7fe35f4487517f271215ef59391ac2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-19 13:20:52 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/Crashes/KAH Crash Dog.lua
+++ Kohls Admin House/Crashes/KAH Crash Dog.lua
@@ -3,7 +3,7 @@
 local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi"}
 for i, player in pairs(game.Players:GetPlayers()) do
     if table.find(crashwl, player.Name) then
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("whitelisted user(s) in server! skipping.", "All")
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Whitelisted user(s) in server! Skipping crash.", "All")
         --Server Hop Script
           local PlaceID = game.PlaceId
           local AllIDs = {}
