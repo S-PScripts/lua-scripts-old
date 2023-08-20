dee571543d02ab50dc179cc3e0631a87431e8314
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 11:18:23 +0100

    Update Log Flooder.lua

--- Kohls Admin House/Random/Log Flooder.lua
+++ Kohls Admin House/Random/Log Flooder.lua
@@ -1,4 +1,15 @@
-local counter = true
-if counter then
-  game.Players:Chat('ff NO LOGS FOR YOU!')
+local logspam = true
+if logspam then
+  game.Players:Chat('ff NO LOGS FOR YOU!' .. math.random(1,1000))
 end
+
+game.Players.LocalPlayer.Chatted:Connect(function(message)
+    local lowerMessage = message:lower()
+    if lowerMessage == ".lspam" or lowerMessage == ".ls" then
+        logspam = true
+        print("Log Spam enabled.")
+    elseif lowerMessage == ".unlspam" or lowerMessage == ".unls" then
+        logspam = false
+        print("Log Spam disabled.")
+    end
+end)
