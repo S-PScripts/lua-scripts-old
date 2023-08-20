b5ec25c37fe3c40fd3b273fc4a730bea26ac5d9d
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 11:44:59 +0100

    Update Annoy People.lua

--- Kohls Admin House/Random/Annoy People.lua
+++ Kohls Admin House/Random/Annoy People.lua
@@ -1,17 +1,22 @@
-local trolling= "me_123eq"
-local spam = true -- change to turn off
-while spam do
-    game.Players:Chat("unchar " .. trolling .. " " .. math.random(1,1000))
-    task.wait(0.5)
-end
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	if string.sub(msg:lower(), 0, 5) == ".spam" then 
+		local trolled = "scri"
+		spam=true
+		
+		while spam == true do
+			game.Players:Chat("unchar " .. trolled .. " " .. math.random(1,1000))
+			wait(0.005)
 
-game.Players.LocalPlayer.Chatted:Connect(function(message)
-    local lowerMessage = message:lower()
-    if lowerMessage == ".spam" or lowerMessage == ".s" then
-        spam = true
-        print("Spam enabled.")
-    elseif lowerMessage == ".unspam" or lowerMessage == ".uns" then
-        spam = false
-        print("Spam disabled.")
-    end
+
+		end
+	end
+end)
+
+
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+	if string.sub(msg:lower(), 0, 5) == ".unspam" then
+		spam = false
+		
+	end
 end)
