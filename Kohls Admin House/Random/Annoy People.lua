cce5ad5e8edac28431964749f23070bdefc1cd01
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 12:11:49 +0100

    Update Annoy People.lua

--- Kohls Admin House/Random/Annoy People.lua
+++ Kohls Admin House/Random/Annoy People.lua
@@ -6,18 +6,12 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 		while spam == true do
 			game.Players:Chat(text)
 			wait(0.1)
+			if string.sub(msg:lower(),0,5) == ".unspam" then
+				local text = ""
+				spam = false
+			end
 
 
 		end
-	end
-end)
-
-
-
-game.Players.LocalPlayer.Chatted:Connect(function(msg)
-	if string.sub(msg:lower(), 0, 5) == ".unspam" then
-		local text = ""
-		spam = false
-		
 	end
 end)
