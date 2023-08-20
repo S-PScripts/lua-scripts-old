8a1d569a3d3efe9d8d87b0d84ecef4556a0067f3
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 16:54:25 +0100

    Create combined.lua

---
+++ Kohls Admin House/Random/combined.lua
@@ -0,0 +1,36 @@
+local spam = false
+local lspam = false
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    local command = string.lower(msg)
+    if command == ".spam" then
+        spam = true
+        print("Spam is on.")
+    end
+    if command == ".unspam" then
+        spam = false
+        print("Spam is off.")
+    end
+    if command == ".lspam" then
+        lspam = true
+        print("Log Spam is on.")
+    end
+    if command == ".unlspam" then
+        lspam = false
+        print("Log Spam is off.")
+    end
+    while true do
+        if spam then
+            game.Players:Chat("char all " .. math.random(1,1000))
+            wait(0.1)
+        else
+            wait(0.1)
+        end
+    while true do
+        if lspam then
+            game.Players:Chat("ff NO LOGS FOR YOU " .. math.random(1,1000))
+            wait(0.1)
+        else
+            wait(0.1)
+        end
+    end
+end)
