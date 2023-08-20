30f428cd3a61aaf61b58fee927c6b8e35a2d9184
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 16:33:22 +0100

    Update Annoy People.lua

--- Kohls Admin House/Random/Annoy People.lua
+++ Kohls Admin House/Random/Annoy People.lua
@@ -1,26 +1,26 @@
-local text = "char all " .. math.random(1,100)
-game.Players.LocalPlayer.Chatted:Connect(function(msg)
-    local command = string.lower(msg)
-    if command == ".spam" then
-        local spam = true
-        print("Spam is on.")
-    end
-end)
-
-game.Players.LocalPlayer.Chatted:Connect(function(msg)
-    local command = string.lower(msg)
-    if command == ".unspam" then
-        local spam = false
-        print("Spam is off.")
-    end
-end)
-
 while true do
-    if spam == true then
+    if spam then
         game.Players:Chat(text)
-        print('PRINTING!')
+        print('I HATE THIS!')
         wait(0.005)
     else
         wait(0.005)
     end
 end
+
+text = "char all " .. math.random(1,100)
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    command = string.lower(msg)
+    if command == ".spam" then
+        spam = true
+        print("Spam is on.")
+    end
+end)
+
+game.Players.LocalPlayer.Chatted:Connect(function(msg)
+    command = string.lower(msg)
+    if command == ".unspam" then
+        spam = false
+        print("Spam is off.")
+    end
+end)
