1c73495db5bce01bc2f1b5b7b14bcc86f1968f72
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 15:52:03 +0100

    Update Log Flooder.lua

--- Kohls Admin House/Random/Log Flooder.lua
+++ Kohls Admin House/Random/Log Flooder.lua
@@ -1,22 +1,21 @@
+local ltext = "FF NO LOGS FOR YOU! " .. math.random(1,100)
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
-	if string.sub(msg:lower(), 0, 5) == ".lspam" then 
-		local logtext = "NO LOGS FOR YOU"
-		logspam=true
-		
-		while logspam == true do
-			game.Players:Chat("ff " .. logtext .. " " .. math.random(1,1000))
-			wait(0.005)
-
-
-		end
-	end
+    local command = string.lower(msg)
+    if command == ".logspam" or command == ".ls" then
+        local lspam = true
+        print("Spam is on.")
+    end
 end)
 
-
-
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
-	if string.sub(msg:lower(), 0, 5) == ".unlspam" then
-		logspam = false
-		
-	end
+    local command = string.lower(msg)
+    if command == ".unlogspam" or command == ".unls" then
+        local lspam = false
+        print("Spam is off.")
+    end
 end)
+
+while lspam do
+   game.Players:Chat(ltext)
+   wait(0.005)
+end
