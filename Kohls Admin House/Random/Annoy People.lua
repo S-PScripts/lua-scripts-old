2e7ca5dd8cd4cd7a612862eca71e1c8fa54bf214
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-20 11:21:46 +0100

    Update Annoy People.lua

--- Kohls Admin House/Random/Annoy People.lua
+++ Kohls Admin House/Random/Annoy People.lua
@@ -2,6 +2,7 @@ local trolling= "me_123eq"
 local spam = false -- change to turn off
 while spam do
     game.Players:Chat("unchar " .. trolling .. " " .. math.random(1,1000))
+    task.wait(0)
 end
 
 game.Players.LocalPlayer.Chatted:Connect(function(message)
