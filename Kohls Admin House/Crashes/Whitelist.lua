2b6934fce8ca9d27bc2af0728796d442ff3a1d8d
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-21 12:18:04 +0100

    Update Whitelist.lua

--- Kohls Admin House/Crashes/Whitelist.lua
+++ Kohls Admin House/Crashes/Whitelist.lua
@@ -2,69 +2,12 @@ local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh
 for i, player in pairs(game.Players:GetPlayers()) do
     if table.find(crashwl, player.Name) then
         game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("whitelisted user(s) in server! skipping.", "All")
-        --Server Hop Script
-          local PlaceID = game.PlaceId
-          local AllIDs = {}
-          local foundAnything = ""
-          local actualHour = os.date("!*t").hour
-          local Deleted = false
-          function TPReturner()
-              local Site;
-              if foundAnything == "" then
-                  Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-              else
-                  Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-              end
-              local ID = ""
-              if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-                  foundAnything = Site.nextPageCursor
-              end
-              local num = 0;
-              for i,v in pairs(Site.data) do
-                  local Possible = true
-                  ID = tostring(v.id)
-                  if tonumber(v.maxPlayers) > tonumber(v.playing) then
-                      for _,Existing in pairs(AllIDs) do
-                          if num ~= 0 then
-                              if ID == tostring(Existing) then
-                                  Possible = false
-                              end
-                          else
-                              if tonumber(actualHour) ~= tonumber(Existing) then
-                                  local delFile = pcall(function()
-                                      AllIDs = {}
-                                      table.insert(AllIDs, actualHour)
-                                  end)
-                              end
-                          end
-                          num = num + 1
-                      end
-                      if Possible == true then
-                          table.insert(AllIDs, ID)
-                          wait()
-                          pcall(function()
-                              wait()
-                              game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-                          end)
-                          wait(4)
-                      end
-                  end
-              end
+        local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
+        for i,v in pairs(Servers.data) do
+          if v.playing ~= v.maxPlayers then
+              game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
           end
-
-          function Teleport()
-              while wait() do
-                  pcall(function()
-                      TPReturner()
-                      if foundAnything ~= "" then
-                          TPReturner()
-                      end
-                  end)
-              end
-          end
-
-          Teleport()
+        end
     end
 end
 
--- ill fix the serverhop soon (using a long one for stuff
