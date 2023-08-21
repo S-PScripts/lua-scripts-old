0f4b23244e2d5105518afd6acedfce993493cc37
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-21 16:37:28 +0100

    Update Kohls+ V1.1.lua

--- Kohls Admin House/Random/Kohls+ V1.1.lua
+++ Kohls Admin House/Random/Kohls+ V1.1.lua
@@ -101,7 +101,7 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
     end
     if command == ".house" then
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-31.0896435, 8.22999477, 70.522644, -0.999961913, 4.495271e-08, -0.0087288795, 4.55292621e-08, 1, -6.58523618e-08, 0.0087288795, -6.62472743e-08, -0.999961913)
-		end
+    end
     if command == ".fixcam" then
 			local lp = PlayerService.LocalPlayer
 			local ui = game:GetService("UserInputService")
