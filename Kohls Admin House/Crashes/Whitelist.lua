8caf2415ff1c20735861901cc5a1b6aae17788c4
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-25 10:49:03 +0100

    Update Whitelist.lua

--- Kohls Admin House/Crashes/Whitelist.lua
+++ Kohls Admin House/Crashes/Whitelist.lua
@@ -1,4 +1,4 @@
-local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi"}
+local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi","thekillercrum","crumsbot2"}
 for i, player in pairs(game.Players:GetPlayers()) do
     if table.find(crashwl, player.Name) then
         game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("whitelisted user(s) in server! skipping.", "All")
@@ -8,6 +8,20 @@ for i, player in pairs(game.Players:GetPlayers()) do
               game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
           end
         end
+    else
+        game.Players:Chat("music 6913550990")
+        wait(0.5)
+        counter = 0
+        while counter < 1000 do
+            game.Players:Chat("clone all all all" .. math.random(1, 1000))
+            game.Players:Chat("dog all all all" .. math.random(1, 1000))
+            counter = counter + 1
+        end
+        local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
+        for i,v in pairs(Servers.data) do
+          if v.playing ~= v.maxPlayers then
+              game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+          end
+        end
     end
 end
-
