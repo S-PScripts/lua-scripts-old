4c259ef10bd4e41db25e2fe226c4022d4585953e
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-25 19:03:50 +0100

    Update KAH Crash Dog.lua

--- Kohls Admin House/Crashes/KAH Crash Dog.lua
+++ Kohls Admin House/Crashes/KAH Crash Dog.lua
@@ -1,29 +1,45 @@
-local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi","thekillercrum","crumsbot2","aligotoofed"}
-for i, player in pairs(game.Players:GetPlayers()) do
-    if table.find(crashwl, player.Name) then
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("{Kohls+ Crasher} Whitelisted user(s) in server! Skipping. RN: ".. math.random(1,1000), "All")
-        local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
-        for i,v in pairs(Servers.data) do
-          if v.playing ~= v.maxPlayers then
-              game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
-          end
-        end
-    else
-        game.Players:Chat("fogend 0")
-        game.Players:Chat("flash")
-        game.Players:Chat("clr")
-        game.Players:Chat("time 0")
-        game.Players:Chat("respawn all")
-        game.Players:Chat("speed all 0")
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Kohls+ Crasher Executed! RN: ".. math.random(1,1000), "All")
-        game.Players:Chat("h \n\n\n Fatal overload 1337. Please reconnect to the server. {Kohls+} \n\n\n")
-        game.Players:Chat("music 6913550990")
-        wait(0.5)
-        counter = 0
-        while counter < 1000 do
-            game.Players:Chat("clone all all all" .. math.random(1, 1000))
-            game.Players:Chat("dog all all all" .. math.random(1, 1000))
-            counter = counter + 1
-        end
-    end
+--IF YOU DONT HAVE PERM ADD LOOPGRAB.lua or PERM.LUA TO THIS SCRIPT
+-- DOESN'T WORK ON KOHLS ADMIN HOUSE BC! PLEASE USE VGCRASH FOR BC.
+-- number = math.random(1, 7)
+game.Players:Chat("fogend 0")
+game.Players:Chat("flash")
+game.Players:Chat("clr")
+game.Players:Chat("time 0")
+game.Players:Chat("respawn all")
+game.Players:Chat("speed all 0")
+game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Kohls+ Crasher Executed! RN: ".. math.random(1,1000), "All")
+game.Players:Chat("h \n\n\n Fatal overload 1337. Please reconnect to the server. {Kohls+} \n\n\n")
+-- if number == 1 then
+--    game.Players:Chat("h \n\n\n Sorry, something went wrong! {ERROR 1} \n\n\n")
+--elseif number == 2 then
+--    game.Players:Chat("h \n\n\n Oops, something went wrong! {ERROR 2} \n\n\n")
+--elseif number == 3 then
+--    game.Players:Chat("h \n\n\n [ROBLOX] Oops, something went wrong! {ERROR 3} \n\n\n")
+--elseif number == 4 then
+--    game.Players:Chat("h \n\n\n [Agspureiam] Oops, something went wrong! {ERROR 4} \n\n\n")
+--elseif number == 5 then
+--    game.Players:Chat("h \n\n\n [ROBLOX] Sorry, something went wrong! {ERROR 5} \n\n\n")
+--elseif number == 6 then
+--    game.Players:Chat("h \n\n\n [Agspureiam] Sorry, something went wrong! {ERROR 6} \n\n\n")
+--else
+--    game.Players:Chat("h \n\n\n Crashed by Dog+Clone. {ERROR 7} \n\n\n")
+--end
+game.Players:Chat("music 6913550990")
+wait(0.5)
+counter = 0
+while counter < 1000 do
+    game.Players:Chat("clone all all all" .. math.random(1, 1000))
+    game.Players:Chat("dog all all all" .. math.random(1, 1000))
+    counter = counter + 1
 end
+-- local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
+-- for i,v in pairs(Servers.data) do
+--  if v.playing ~= v.maxPlayers then
+--      game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+--  end
+-- end
+
+-- 0000000000000000000009048375035 (all dropping 8 beats)
+-- 0000000000000000000006680495507 (meow meow)
+-- 0000000000000000000006529070845 (loud)
+-- 0000000000000000000006917155909 (they trying to be cray)
