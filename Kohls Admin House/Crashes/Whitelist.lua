2ad4deb28f222b1ed13d893f46bfd2d445908a54
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-25 19:04:59 +0100

    Update Whitelist.lua

--- Kohls Admin House/Crashes/Whitelist.lua
+++ Kohls Admin House/Crashes/Whitelist.lua
@@ -1,7 +1,7 @@
 local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi","thekillercrum","crumsbot2","aligotoofed"}
 for i, player in pairs(game.Players:GetPlayers()) do
     if table.find(crashwl, player.Name) then
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("{Kohls+ Crasher} Whitelisted user(s) in server! Skipping. RN: ".. math.random(1,1000), "All")
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("{Kohls+ Crasher} Whitelisted user(s) in server! RN: ".. math.random(1,1000), "All")
         local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
         for i,v in pairs(Servers.data) do
           if v.playing ~= v.maxPlayers then
@@ -15,7 +15,7 @@ for i, player in pairs(game.Players:GetPlayers()) do
         game.Players:Chat("time 0")
         game.Players:Chat("respawn all")
         game.Players:Chat("speed all 0")
-        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Kohls+ Crasher Executed! RN: ".. math.random(1,1000), "All")
+        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("{Kohls+ Crasher} No whitelisted users found in server! RN: ".. math.random(1,1000), "All")
         game.Players:Chat("h \n\n\n Fatal overload 1337. Please reconnect to the server. {Kohls+} \n\n\n")
         game.Players:Chat("music 6913550990")
         wait(0.5)
