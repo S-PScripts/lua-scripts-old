d1f13500194b21739785ac87338a2d91acd57aea
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-26 13:59:15 +0100

    Update KAH Dog Sil.lua

--- Kohls Admin House/Crashes/KAH Dog Sil.lua
+++ Kohls Admin House/Crashes/KAH Dog Sil.lua
@@ -1,8 +1,9 @@
 --ONLY WORKS IN KAH NBC
---IF YOU DONT HAVE PERM ADD THE KAH PADS.lua CFRAMES TO THIS SCRIPT
-counter = 0
-while counter < 1000 do
-  game.Players:Chat('clone all all all')
-  game.Players:Chat('dog all all all')
-  counter = counter + 1
-end
+--IF YOU DONT HAVE PERM USE LG/PERM IN PADS FOLDER
+task.spawn(function()
+    while true do
+        game.Players:Chat("dog all all all")
+        game.Players:Chat("clone all all all")
+        task.wait()
+    end
+end)
