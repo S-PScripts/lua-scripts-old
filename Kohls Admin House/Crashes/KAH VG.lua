a92e5037ade0c52d6b00aa457012e59beb68c343
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-26 13:56:58 +0100

    Update KAH VG.lua

--- Kohls Admin House/Crashes/KAH VG.lua
+++ Kohls Admin House/Crashes/KAH VG.lua
@@ -21,3 +21,10 @@ task.spawn(function()
         task.wait()
     end
 end)
+
+-- local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
+-- for i,v in pairs(Servers.data) do
+--  if v.playing ~= v.maxPlayers then
+--      game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+--  end
+-- end
