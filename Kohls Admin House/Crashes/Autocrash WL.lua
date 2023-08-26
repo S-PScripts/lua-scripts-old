980f63d1870ea813d73bc6c2780aabac78d0c6a6
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-08-26 13:58:27 +0100

    Update Autocrash WL.lua

--- Kohls Admin House/Crashes/Autocrash WL.lua
+++ Kohls Admin House/Crashes/Autocrash WL.lua
@@ -2,6 +2,7 @@ local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh
 for i, player in pairs(game.Players:GetPlayers()) do
     if table.find(crashwl, player.Name) then
        continue = false
+       game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("{Kohls+ Crasher} Whitelisted user(s) found in server! RN: ".. math.random(1,1000), "All")
        local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
 	   for i,v in pairs(Servers.data) do
   	       if v.playing ~= v.maxPlayers then
