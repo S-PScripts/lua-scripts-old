83ad71d8dce0f83487c8ecccef364fcfe030eb79
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-09-02 21:48:07 +0100

    Create techcrash.lua

---
+++ Kohls Admin House/techcrash.lua
@@ -0,0 +1,9 @@
+for i = 1,52 do
+    game.Players:Chat("dog all all all                                                                           fuck")
+end
+for i = 1,51 do
+    game.Players:Chat("clone all all all                                                                           fuck")
+end
+while task.wait() do
+    game.Players:Chat("clone all all all                                                                           fuck")
+end
