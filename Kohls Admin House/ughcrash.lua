89c6c8bc40a08655f05937c76e9afdad9d769d60
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-09-02 21:47:13 +0100

    Create ughcrash.lua

---
+++ Kohls Admin House/ughcrash.lua
@@ -0,0 +1,8 @@
+for i = 0,5 do
+for i = 0,3 do game.Players:Chat("size all .3") end
+for i = 0,2 do game.Players:Chat("freeze all all all") end
+game.Players:Chat("size all 9.99")
+game.Players:Chat("dog all all all")
+game.Players:Chat("size all 10")
+for i = 0, 4 do game.Players:Chat("clone all all all") end
+game.Players:Chat("unsize all all all")
