3b19705d3edadd22015cd74ed711c1889d7418d2
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-09-02 21:47:37 +0100

    Create kozycrash.lua

---
+++ Kohls Admin House/kozycrash.lua
@@ -0,0 +1,30 @@
+function knockssilcrashidkitskindatrashtbh()
+for i=1,5 do
+game.Players:Chat("size all .3")
+end
+for i=1,13 do
+game.Players:Chat("rocket/all all all")
+game.Players:Chat("freeze all all all")
+game.Players:Chat("dog all all all")
+end
+for i=1,5 do
+game.Players:Chat("size all 10")
+end
+for i=1,200 do
+game.Players:Chat("clone all all all")
+end
+while true do task.wait()
+game.Players:Chat("clone all all all")
+end
+end
+_G.rapelistblackiesandslavesonly = {"sgoslee", "sgoslee619", "goku"}
+for i,v in ipairs(_G.rapelistblackiesandslavesonly) do
+if game.Players:FindFirstChild(v) then
+knockssilcrashidkitskindatrashtbh()
+end
+end
+game.Players.PlayerAdded:Connect(function(v)
+if table.find(_G.rapelistblackiesandslavesonly, v.Name) then
+knockssilcrashidkitskindatrashtbh()
+end
+end)
