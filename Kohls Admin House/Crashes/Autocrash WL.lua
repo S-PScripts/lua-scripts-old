7353f9a9753864273ecdada644c5551a93644f81
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-09-06 16:20:23 +0100

    Update Autocrash WL.lua

--- Kohls Admin House/Crashes/Autocrash WL.lua
+++ Kohls Admin House/Crashes/Autocrash WL.lua
@@ -1,24 +1,24 @@
-local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi","thekillercrum","crumsbot2","aligotoofed","sneakcal264"}
+--local crashwl = {"t_echr", "BANNter_Original","SZCVAK", "Di33le2","Altsarecooleh","MrPetDog12345","Phoenictron","Ripend","Imaimashi","thekillercrum","crumsbot2","aligotoofed","sneakcal264"}
 continue = false
-for i, player in pairs(game.Players:GetPlayers()) do
-    if table.find(crashwl, player.Name) then
-       continue = true
-       local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
-	   for i,v in pairs(Servers.data) do
-  	       if v.playing ~= v.maxPlayers then
-      	      game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
-           end
-       end
-   end
-end
+--for i, player in pairs(game.Players:GetPlayers()) do
+--    if table.find(crashwl, player.Name) then
+--       continue = true
+--       local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/112420803/servers/Public?sortOrder=Asc&limit=100"))
+--	   for i,v in pairs(Servers.data) do
+--  	       if v.playing ~= v.maxPlayers then
+--      	      game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
+--           end
+--       end
+--   end
+--end
 
-if continue == false then
+--if continue == false then
     game.Players:Chat("clr")
     game.Players:Chat("fix")
     game.Players:Chat("time 0")   
     game.Players:Chat("respawn all")
     game.Players:Chat("speed all 0")
-    game.Players:Chat("h me, ts2021, is a skid (crashing because of don and kozy)")
+    game.Players:Chat("h me, ts2021, is a skid")
     game.Players:Chat("music 00000000000000000000006772846771")
     wait(0.5)
     counter = 0
@@ -33,4 +33,4 @@ if continue == false then
   --	       game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
 --        end
 --    end
-end
+--end
