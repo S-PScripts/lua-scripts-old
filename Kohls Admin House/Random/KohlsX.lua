ddc980df411f9c6a53d8b5d5be5cbf8c860db5a3
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-09 19:28:18 +0100

    Update KohlsX.lua

--- Kohls Admin House/Random/KohlsX.lua
+++ Kohls Admin House/Random/KohlsX.lua
@@ -4,3 +4,258 @@ Hierarchy:
 -- KohlsU (KohlsUnlimited)
 -- KohlsX (KohlsInfinity)
 -- KohlsP (KohlsPremium) 
+-- KohlsF (KohlsFun)
+
+Features:
+Antis- (ORDERED BAD :( )
+-- AntiPunish
+-- AntiExplode
+-- AntiKill / NOK
+-- AntiJail
+-- AntiSpeed
+-- AntiJump
+-- AntiBlind
+-- AntiFly
+-- AntiGod
+-- AntiFF/Forcefield
+-- AntiRespawn
+-- AntiReset/Refresh/Reload
+-- AntiGrav
+-- AntiFogend/Fogstart/Ambient/ColourShift
+-- AntiFlash/Disco/Time
+-- AntiTP
+-- AntiTrip
+-- AntiStun
+-- AntiSeizure
+-- AntiFreeze
+-- AntiLogs
+-- Anti H/M/PM
+-- AntiPaint
+-- AntiSkydive
+-- AntiGlow/Trail/Particle
+-- AntiInvis/Fire/Bonfire/Smoke/Sparkles
+-- AntiEffect (ify)/AntiPackage
+-- AntiHead
+-- AntiSpin
+-- AntiCape/Hat/Shirt/Pants/Face
+-- AntiClone
+-- AntiDog/Creeper
+-- AntiChar
+-- AntiFling
+-- AntiMusic
+-- AntiLock
+-- AntiNoclip
+-- AntiSize
+-- AntiHeal/Damage/SetHealth
+-- AntiGear
+-- AntiGearBan
+-- AntiSword
+-- AntiCrash
+-- AntiKick
+-- AntiName
+-- AntiClear
+
+Part Related:
+-- MoveBaseplate/MoveBP
+-- MoveFloors/MoveFl
+-- MoveHouseFloors/MoveHF
+-- MoveHouseFloor1/MOVEHF1
+-- MoveHouseFloor2/MOVEHF2
+-- MoveGrass
+-- MoveObbyWalls/MoveOW
+-- MoveObby
+-- MoveObbyBricks/MoveOB
+-- MoveHouse
+-- MoveAdminPads/MoveAP
+-- MoveAdminPad()/MoveAP()
+-- MoveAdminDividers/MoveAD
+-- MoveRegen
+-- MoveHouseWalls/MoveHW
+-- MoveHouseRoof/MoveHR
+-- MoveBricks
+-- MoveAll
+
+-- FindBaseplate/FindBP
+-- FindFloors/FindFl
+-- FindHouseFloors/FindHF
+-- FindHouseFloor1/FINDHF1
+-- FindHouseFloor2/FindHF2
+-- FindGrass
+-- FindObbyWalls/FindOW
+-- FindObby
+-- FindObbyBricks/FindOB
+-- FindHouse
+-- FindAdminPads/FindAP
+-- FindAdminPad()/FindAP()
+-- FindAdminDividers/FindAD
+-- FindRegen
+-- FindHouseWalls/FindHW
+-- FindHouseRoof/FindHR
+-- FindBricks
+-- FindAll
+
+-- FixBaseplate/FixBP
+-- FixFloors/FixFl
+-- FixHouseFloors/FixHF
+-- FixHouseFloor1/FixHF1
+-- FixHouseFloor2/FixHF2
+-- FixGrass
+-- FixObbyWalls/FixOW
+-- FixObby
+-- FixObbyBricks/FixOB
+-- FixHouse
+-- FixAdminPads/FixAP
+-- FixAdminPad()/FixAP()
+-- FixAdminDividers/FixAD
+-- FixRegen
+-- FixHouseWalls/FixHW
+-- FixHouseRoof/FixHR
+-- FixBricks
+
+Other Fixes:
+-- FixPaint
+
+Music:
+-- GetMusicID/Name
+-- WLMusicID ()
+-- BLMusicID ()
+-- MyMusicOnly () -- restarts music if changed
+-- PauseMusic
+-- PlayMusic
+-- MusicFW ()
+-- MusicBW ()
+-- RestartMusic
+-- MusicPoint ()
+-- MusicLog
+
+Music Playlist:
+-- PlaylistON/OFF
+-- MusicPL (1,2,3,4,5,6,7,8,9,10...)
+-- NextMusic
+-- PreviousMusic
+-- ToStart
+-- ToEnd
+-- ToRandom
+-- Shuffle
+
+Perm: -- (give everyone admin)
+-- perm
+-- unperm
+
+Whitelist: -- (gives people ability to go around antis on)
+-- wl
+-- unwl
+-- editwl (edit the whitelisted commands)
+
+Blacklist:
+-- bl
+-- unbl
+-- slock (serverlock, like bl all)
+-- unslock
+-- bypslock (whitelist for people to not get slock)
+-- unbypslock
+
+Admin: -- (give people ability to have commands in KohlsX)
+-- admin
+-- unadmin
+-- editadmin
+
+Detections:
+-- getadmins
+-- get perm admins
+-- get persons admins
+-- getgamepassers
+-- getnongamepassers
+-- getwhitelisted
+-- getblacklisted
+-- getranked
+-- getkohlsxusers
+
+Builder:
+-- build (a build that is saved to here, it gets the parts to make a model of something)
+-- drawon (free draw)
+-- drawoff
+-- dcolour (a colour for the draw)
+
+Themes:
+-- theme () (theme that is saved here, it changes ambient, fogend, does music etc.)
+
+Gear Related:
+-- blgear (id)
+-- wlgear (id)
+-- gearban (plr)
+-- reversegb on/off
+
+Pad Related:
+-- getpad (1 to 9)
+-- allpads
+-- permpad (1 to 9)
+-- loopgrab
+-- regen
+-- spamregen
+-- unspamregen
+-- unlg
+-- unperm
+
+TPs:
+-- house
+-- spawn
+-- roof
+-- bricks
+
+Spam:
+-- Super (a command) (times of repeat)
+-- AddSpam (spam something)
+-- SpamList
+-- ClrList
+-- Unspam (The Spam ID)
+
+Search:
+-- searchgear (Name here, searches on the store)
+-- searchmusic (name here)
+-- searchchar (finds char of a user)
+
+Words:
+-- music (name) [so it searches it then gives it]
+-- gear (name) 
+-- char (name)
+
+Plugins:
+-- addcommand (This would be in a GUI. You put a name and the loadstring/lua code for it)
+-- removecommand (id)
+-- plcommandlist
+-- clrcmdlist
+
+Settings:
+-- commandlist
+-- prefix (the prefix)
+-- theme (for gui)
+
+Kick:
+-- rkick
+-- gearkick (id) -- if a gear is found that can crash with gears again...
+-- reversekick on/off
+
+Crash:
+-- vgcrash
+-- percrash
+-- gearcrash (id)
+-- dogcrash 
+-- freezecrash
+
+Lag:
+-- lag (with the car id)
+-- unlag
+
+Announcement:
+-- permh (something)
+-- unpermh
+-- spam m/pm (something)
+-- unspam m/pm
+
+Hidden Announcement:
+-- hpermh (something)
+-- unhpermh
+-- spoofusername (a username that comes before announcement)
+
+finishing soon :)
