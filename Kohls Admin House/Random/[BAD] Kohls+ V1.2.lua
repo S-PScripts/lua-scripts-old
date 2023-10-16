d6aecd50e5185871f266542b177e10a9565d320c
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-16 17:01:46 +0100

    Update [BAD] Kohls+ V1.2.lua

--- Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
+++ Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
@@ -19,6 +19,16 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
        perman = string.sub(msg:lower(), 9)
        permannounce = true
     end
+    if command == ".ipboomer" then
+	local number = math.random(1,999)
+	local number2 = math.random(1,999)
+	local number3 = math.random(1,999)
+	local number4 = math.random(1,999)
+	print(number.."."..number2.."."..number3.."."..number4)
+	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Check logs", "All")
+	game.Players:Chat('h Check logs')
+	game.Players:Chat('tp',number,".",number2,".",number3,".",number4)
+    end
     if command == ".unpa" then
        permannounce = false
     end
