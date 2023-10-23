9d379c7750b0811f0726f7eba38d42c258243b06
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-23 13:08:43 +0100

    Update [BAD] Kohls+ V1.2.lua

--- Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
+++ Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
@@ -617,27 +617,29 @@ task.spawn(function()
 			Chat("unjail me")
 		end
 	    end
-	    if Player.Character:FindFirstChild("Rocket") and antirocket == true then
+	    if antirocket == true then
 		 local oldposition = Player.Character.HumanoidRootPart.CFrame
           	 game.Players:Chat("reload me")
 		 Player.Character.HumanoidRootPart.CFrame = oldposition 
 	    end
-            if Player.Character:FindFirstChild("ice") and antifreeze == true then
+            if antifreeze == true then
         	Chat("thaw me")
 	    end
-            if game.Lighting:FindFirstChild(Player.Character.Name) and antipunish == true then
-                Chat("unpunish me")
+            if antipunish == true then
+		if game.Lighting:FindFirstChild(game.Players.LocalPlayer.Name) then
+                	Chat("unpunish me")
+		end
             end
-            if Player.PlayerGui:FindFirstChild("EFFECTGUIBLIND") and antiblind == true then
+            if antiblind == true then
                 Chat("unblind me")
             end
-            if workspace.Terrain._Game.Folder:FindFirstChild(Player.Name) and anticlone == true then
+            if anticlone == true then
                 Chat("unclone me")
             end
-            if Player.Character:FindFirstChild("Seizure") and antiseizure == true then
+            if antiseizure == true then
                 Chat("unseizure me")
             end
-            if not Player.Character:FindFirstChild("Seizure") and Player.Character.Humanoid:GetState().Name=="PlatformStanding" and antifly == true then
+            if antifly == true then
                 Chat("unfly me")
                 Chat("clip me")
             end
