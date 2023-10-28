9dc94fddf174cde49aa4da493676ae0954936db9
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-28 20:26:52 +0100

    Update [BAD] Kohls+ V1.2.lua

--- Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
+++ Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
@@ -741,6 +741,8 @@ function start(plr)
 					end					
 				end
 			end
+		end)
+		task.spawn(function()
 			if string.sub(msg:lower(),0,5) == "music" or string.sub(msg:lower(),0,6) == ":music" or string.sub(msg:lower(),0,3) == "clr" or string.sub(msg:lower(),0,4) == ":clr" or string.sub(msg:lower(),0,5) == "clear" or string.sub(msg:lower(),0,6) == ":clear" or string.sub(msg:lower(),0,6) == ":clean" or string.sub(msg:lower(),0,5) == "clean" then
 				if antimusic == true then
 					if plr.Name == game.Players.LocalPlayer.Name then
