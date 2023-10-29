c535af179dc323595133dd06b7b0e1d7b548fc0c
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-29 15:03:53 +0000

    Create iaosfdsfdofdsa.lua

---
+++ Kohls Admin House/Random/iaosfdsfdofdsa.lua
@@ -0,0 +1,11 @@
+multianti = true
+local function ultanti(msg)
+  while multianti == true do
+      task.wait()
+      if nopunish == true then
+          if game.Lighting:FindFirstChild(game.Players.LocalPlayer.Name) then
+              Chat("unpunish me")
+          end
+      end
+  end
+end
