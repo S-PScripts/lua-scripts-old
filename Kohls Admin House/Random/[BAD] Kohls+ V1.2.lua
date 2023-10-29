c7c3401a420a16bfce92bf815472c7348f707311
S-PScripts <136199723+S-PScripts@users.noreply.github.com>
2023-10-29 10:45:54 +0000

    Oh, that's how

--- Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
+++ Kohls Admin House/Random/[BAD] Kohls+ V1.2.lua
@@ -664,92 +664,100 @@ task.spawn(function()
 	    if nameshart == true then
 	       Chat("name all Broken")
 	    end
-	    if antichat == true then
+      end
+end)()
+
+-- this is for testing functions!
+local function Antis()
+  while true do
+  task.wait()
+	if antichat == true then
 		Chat("h \n\n\n 😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀😀😃😄😁😆😅😂🤣😭💀💀💀💀💀💀💀💀💀 \n\n\n")
             end
-	    if antijail == true then
+	if antijail == true then
 		if game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild(game.Players.LocalPlayer.Name.."'s jail") then
 			Chat("unjail me")
 		end
 	    end
-	    if antirocket == true then
+	if antirocket == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("Rocket") then
 		 	local oldposition = Player.Character.HumanoidRootPart.CFrame
           	 	game.Players:Chat("reload me")
 		 	Player.Character.HumanoidRootPart.CFrame = oldposition 
 		end
 	    end
-            if antifreeze == true then
+        if antifreeze == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("ice") then
         		Chat("thaw me")
 		end
 	    end
-            if antipunish == true then
+        if antipunish == true then
 		if game.Lighting:FindFirstChild(game.Players.LocalPlayer.Name) then
                 	Chat("unpunish me")
 		end
             end
-            if antiblind == true then
+	if antiblind == true then
 		for i, v in pairs(game.Players.LocalPlayer.PlayerGui:GetDescendants()) do
                     if v.Name == "EFFECTGUIBLIND" then
 			 Chat("unblind me")
                     end
 		end
             end
-            if anticlone == true then
+        if anticlone == true then
 		if game:GetService("Workspace").Terrain["_Game"].Folder:FindFirstChild(game.Players.LocalPlayer.Name) then
                 	Chat("unclone me")
 		end
             end
-            if antiseizure == true then
+        if antiseizure == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("Seizure") then
                 	Chat("unseizure me")
 		end
             end
-            if antifly == true then
+        if antifly == true then
 		if not game.Players.LocalPlayer.Character:FindFirstChild("Seizure") and game.Players.LocalPlayer.Character.Humanoid:GetState().Name=="PlatformStanding" then
                 	Chat("unfly me")
                 	Chat("clip me")
 		end
-            end
-	    if antikill == true then
+        end
+	if antikill == true then
     		if game.Players.LocalPlayer.Character then
         		if game.Players.LocalPlayer.Character.Humanoid.Health == 0 then
            			 Chat("reset me")
         		end
     		end
-	    end
-	    if antiswag == true then
+	end
+	if antiswag == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("EpicCape") then
 			Chat("normal me")
 		end
-	    end
-	   if antisparkles == true then
+	end
+	if antisparkles == true then
 		if game.Players.LocalPlayer.Character.Torso:FindFirstChild("Sparkles") then
 			Chat("unsparkles me")
 		end
-	   end
-	   if antifire == true then
+	end
+	if antifire == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("Torso") then
 			if game.Players.LocalPlayer.Character.Torso:FindFirstChild("Fire") then
 				Chat("unfire me")
 			end
 		end
-	   end	
-	   if antismoke == true then
+	end	
+	if antismoke == true then
 		if game.Players.LocalPlayer.Character:FindFirstChild("Torso") then
 			if game.Players.LocalPlayer.Character.Torso:FindFirstChild("Smoke") then
 				Chat("unsmoke me")
 			end
 		end
-	   end
-	  if antispeed == true then
+	end
+	if antispeed == true then
 		if not game.Players.LocalPlayer.Character.Humanoid.WalkSpeed == 16 then
 			game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 		end
-	  end
-      end
-end)()
+	end
+  end
+end
+Antis()
 
 -- this is for the anti attach
 function start(plr)
@@ -798,5 +806,5 @@ function start(plr)
 		end)
 	end)
 end
-
+start()
 -- for the antis
